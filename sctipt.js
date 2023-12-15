6e630a1dcf8d38ee0f320f15c913c19fe61eefc9
Said <saidsaid01sm@gmail.com>
2023-12-15 19:29:16 +0500

    first

---
+++ sctipt.js
@@ -0,0 +1,8 @@
+// Запрашиваем у пользователя три числа
+var number1 = parseFloat(prompt("Введите первое число:"));
+var number2 = parseFloat(prompt("Введите второе число:"));
+var number3 = parseFloat(prompt("Введите третье число:"));
+
+    var average = (number1 + number2 + number3) / 3;
+
+    console.log("Среднее значение трех чисел: " + average);
